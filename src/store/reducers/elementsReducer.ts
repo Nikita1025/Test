c70f7c4bef4d0ef4650338ebb5088752fd6609fb
Nikita1025 <nekit1025l@gmail.com>
2023-03-13 19:32:20 +0300

    commit

--- src/store/reducers/elementsReducer.ts
+++ src/store/reducers/elementsReducer.ts
@@ -1,100 +1,85 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    calculator: [{
-        count: {
-            position: 1,
-            data: {
-                count: 0
-            }
-        },
-        operationButtons: {
-            position: 2,
-            data: [
-                {id: 1, symbol: '/'},
-                {id: 2, symbol: 'x'},
-                {id: 3, symbol: '-'},
-                {id: 4, symbol: '+'},
-            ]
-        },
-        numbers: {
-            position: 3,
-            data: [
-                {id: 1, number: 9},
-                {id: 2, number: 8},
-                {id: 3, number: 7},
-                {id: 4, number: 6},
-                {id: 5, number: 5},
-                {id: 6, number: 4},
-                {id: 7, number: 3},
-                {id: 8, number: 2},
-                {id: 9, number: 1},
-                {id: 10, number: ','},
-                {id: 11, number: 0},
-
-            ]
-        },
-        result: {
-            position: 4,
-            data: null
-        }
-    }],
-    newCalculator: [{
-        count: {
-            position: 1,
-            data: {count: null}
-        },
-        operationButtons: {
-            position: 2,
-            data: [
-                {id: 1, symbol: '/'},
-                {id: 2, symbol: 'x'},
-                {id: 3, symbol: '-'},
-                {id: 4, symbol: '+'},
-            ]
-        },
-        numbers: {
-            position: 3,
-            data: [
-                {id: 1, number: null},
-                {id: 2, number: null},
-                {id: 3, number: null},
-                {id: 4, number: null},
-                {id: 5, number: null},
-                {id: 6, number: null},
-                {id: 7, number: null},
-                {id: 8, number: null},
-                {id: 9, number: null},
-                {id: 10, number: null},
-                {id: 11, number: null},]
-        },
-        result: {
-            position: 4,
-            data: null
-        }
-    }]
+interface CalcElement {
+  id: number;
+  value: string | number;
 }
 
-const slice = createSlice({
-    name: 'calculator',
-    initialState: initialState,
-    reducers: {
-        changeCalculatorAC(state, action: any) {
-            state.newCalculator = action.newElement
+export interface CalcBlock {
+  position: number;
+  label: string;
+  data: CalcElement[];
+}
+
+export interface Calc {
+  id: number;
+  data: CalcBlock[];
+}
+
+interface InitialState {
+  calculators: Calc[];
+}
+
+const initialState: InitialState = {
+  calculators: [
+    {
+      id: 1,
+      data: [
+        {
+          position: 1,
+          label: 'count',
+          data: [{ id: 1, value: 0 }],
         },
-        sortAC(state,action:any){
-            state.newCalculator.map(el=>{
-                if(el.operationButtons.position === action.operationButtons.position){
-                    return el.operationButtons.position = action.operationButtons.position
-                }
-                return el
-            })
-        }
-    }
-})
+        {
+          position: 2,
+          label: 'operationButtons',
+          data: [
+            { id: 1, value: '/' },
+            { id: 2, value: 'x' },
+            { id: 3, value: '-' },
+            { id: 4, value: '+' },
+          ],
+        },
+        {
+          position: 3,
+          label: 'numbers',
+          data: [
+            { id: 1, value: 9 },
+            { id: 2, value: 8 },
+            { id: 3, value: 7 },
+            { id: 4, value: 6 },
+            { id: 5, value: 5 },
+            { id: 6, value: 4 },
+            { id: 7, value: 3 },
+            { id: 8, value: 2 },
+            { id: 9, value: 1 },
+            { id: 10, value: ',' },
+            { id: 11, value: 0 },
+          ],
+        },
+        {
+          position: 4,
+          label: 'result',
+          data: [{ id: 1, value: '=' }],
+        },
+      ],
+    },
+    {
+      id: 2,
+      data: [] as CalcBlock[],
+    },
+  ],
+};
 
-export const elementsReducer = slice.reducer
-export const {changeCalculatorAC, sortAC} =slice.actions
+const slice = createSlice({
+  name: 'calculator',
+  initialState: initialState,
+  reducers: {
+    setCalcBlock: (state, action) => {
+      state.calculators = action.payload;
+    },
+  },
+});
 
-
-
+export const elementsReducer = slice.reducer;
+export const { setCalcBlock } = slice.actions;
